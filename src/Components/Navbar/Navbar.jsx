cfa6f99ee922264530b40b874a165fcc5b0158df
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-02-26 11:48:41 +0100

    Home Page

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <img className="w-5 mr-2" src={Facebook} alt="Facebook" />
+                    <img className="w-6 mr-2" src={Facebook} alt="Facebook" />
                   </a>
                 </div>
                 <div>
@@ -65,70 +65,71 @@ const Navbar = () => {
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <img className="w-5" src={Instagram} alt="Instagram" />
+                    <img className="w-6" src={Instagram} alt="Instagram" />
                   </a>
                 </div>
               </div>
             </div>
           </div>
-          <div>
-            <div className="flex flex-row-reverse items-center justify-between lg:hidden">
-              <div className={nav ? "visible lg:invisible" : "invisible"}>
-                <img src={Logo} className="w-24" alt="Logo" />
-              </div>
-              <div>
-                <div className="lg:hidden">
-                  <div className="cursor-pointer">
-                    {nav ? (
-                      <AiOutlineMenu className="w-6 h-6" onClick={navHandler} />
-                    ) : (
-                      <FaXmark className="w-6 h-6" onClick={navHandler} />
-                    )}
-                  </div>
+        </div>
+
+        <div>
+          <div className="flex flex-row-reverse items-center justify-between px-8 lg:hidden">
+            <div className={nav ? "visible lg:invisible" : "invisible"}>
+              <img src={Logo} className="w-24" alt="Logo" />
+            </div>
+            <div>
+              <div className="lg:hidden">
+                <div className="cursor-pointer">
+                  {nav ? (
+                    <AiOutlineMenu className="w-6 h-6" onClick={navHandler} />
+                  ) : (
+                    <FaXmark className="w-6 h-6" onClick={navHandler} />
+                  )}
                 </div>
               </div>
             </div>
-            <div className="relative">
-              <div
-                className={
-                  nav
-                    ? "absolute w-full left-[-150%] duration-500"
-                    : "duration-500 absolute left-0 mr-4 w-full bg-grey shadow-2xl z-50"
-                }
-              >
-                <div className="flex flex-col p-4 mr-4">
-                  {navbarItems.map((el, i) => (
-                    <div key={i} className="my-4">
-                      <Link className="link" to={navbarLinks[i]}>
-                        {el}
-                      </Link>
-                    </div>
-                  ))}
-
-                  <div className="flex gap-2 mt-4">
-                    <a
-                      href="https://web.facebook.com/dzworkaway"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <img className="w-5" src={Facebook} alt="Facebook" />
-                    </a>
-
-                    <a
-                      href="https://www.instagram.com/dzworkaway/"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <img className="w-5" src={Instagram} alt="Instagram" />
-                    </a>
+          </div>
+          <div className="relative">
+            <div
+              className={
+                nav
+                  ? "absolute w-full left-[-150%] duration-500 z-50"
+                  : "absolute w-full left-0 bg-grey shadow-2xl z-50 duration-500"
+              }
+            >
+              <div className="flex flex-col p-8">
+                {navbarItems.map((el, i) => (
+                  <div key={i} className="my-4">
+                    <Link className="link" to={navbarLinks[i]}>
+                      {el}
+                    </Link>
                   </div>
-                  <div className="mx-auto mt-2 lg:hidden">
-                    <img
-                      className={nav ? "invisible w-24" : "visible w-24"}
-                      src={Logo}
-                      alt="Logo"
-                    />
-                  </div>
+                ))}
+
+                <div className="flex gap-2 mt-4">
+                  <a
+                    href="https://web.facebook.com/dzworkaway"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img className="w-5" src={Facebook} alt="Facebook" />
+                  </a>
+
+                  <a
+                    href="https://www.instagram.com/dzworkaway/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img className="w-5" src={Instagram} alt="Instagram" />
+                  </a>
+                </div>
+                <div className="mx-auto mt-2 lg:hidden">
+                  <img
+                    className={nav ? "invisible w-24" : "visible w-24"}
+                    src={Logo}
+                    alt="Logo"
+                  />
                 </div>
               </div>
             </div>
