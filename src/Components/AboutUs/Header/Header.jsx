f303c0ba80e64d3f4f46d2b78b6d2ac1543bcbcc
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-02-29 14:56:22 +0100

    About Us Header Section

--- src/Components/AboutUs/Header/Header.jsx
+++ src/Components/AboutUs/Header/Header.jsx
@@ -1,5 +1,30 @@
+import Img from "../../../assets/AboutUsBack.jpg";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="relative">
+      <div>
+        <img
+          src={Img}
+          className="w-full md:h-[35rem]"
+          alt="About US Background"
+        />
+      </div>
+      <div className="absolute pl-8 text-white -translate-y-1/4 md:pl-24 top-1/2">
+        <div>
+          <h1>About Us</h1>
+        </div>
+        <div className="flex mt-1 md:mt-2">
+          <div className="w-8 h-1 md:w-12 bg-secondColor"></div>
+          <div className="flex ml-1">
+            <div className="w-[3px] h1 bg-secondColor"></div>
+            <div className="w-[3px] mx-1 h1 bg-secondColor"></div>
+            <div className="w-[3px] h1 bg-secondColor"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
