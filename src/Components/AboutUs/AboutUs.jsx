912b828b3abd30f55521d641f2c34fa806a64af2
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-03-02 11:31:17 +0100

    About Us Introduction Section

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -1,10 +1,14 @@
 import Header from "./Header/Header";
+import Introduction from "./Introduction/Introduction";
 
 const AboutUs = () => {
   return (
     <>
       <div>
         <Header />
+        <div className="container">
+          <Introduction />
+        </div>
       </div>
     </>
   );
