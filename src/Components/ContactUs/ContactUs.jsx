a1303c2b167a9ce8811d6bd94b53570847b78397
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-03-04 11:09:28 +0100

    Contact Us GetIn Touch Section

--- src/Components/ContactUs/ContactUs.jsx
+++ src/Components/ContactUs/ContactUs.jsx
@@ -1,11 +1,14 @@
 import Header from "./Header/Header";
+import GetInTouch from "./GetInTouch/GetInTouch";
 
 const ContactUs = () => {
   return (
     <>
-      <div className="container"></div>
       <div>
         <Header />
+        <div className="container">
+          <GetInTouch />
+        </div>
       </div>
     </>
   );
