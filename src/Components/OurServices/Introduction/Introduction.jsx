17309de6988fe6ce895b3259927055f321da8421
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-03-07 20:24:44 +0100

    Adding Translation

--- src/Components/OurServices/Introduction/Introduction.jsx
+++ src/Components/OurServices/Introduction/Introduction.jsx
@@ -1,4 +1,24 @@
+import Data from "../../../data.json";
 import { motion } from "framer-motion";
+
+const HeadingsAndParagraphs =
+  Data.OurServices_Introduction__Heading_Paragraph.map(
+    ({ id, header, description }) => {
+      return (
+        <div key={id}>
+          <div>
+            {" "}
+            <div>
+              <h1 className="relative headingStyleLg">{header}</h1>
+            </div>
+            <div>
+              <p>{description}</p>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  );
 
 const Introduction = () => {
   return (
@@ -15,21 +35,7 @@ const Introduction = () => {
         className="mt-12 md:mt-20"
       >
         <div>
-          <div className="w-full md:w-3/4">
-            <div>
-              <h1 className="relative headingStyleLg">
-                Welcome to DZ Work Away Immigration Services
-              </h1>
-            </div>
-            <div>
-              <p>
-                At DZ Work Away, we are your trusted guide through immigration.
-                Whether it is a work visa, family reunion, or residency, we are
-                here to simplify the process with expertise and personalized
-                support.
-              </p>
-            </div>
-          </div>
+          <div className="w-full md:w-3/4">{HeadingsAndParagraphs}</div>
         </div>
       </motion.div>
     </>
