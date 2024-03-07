56eb19d038d57c523eaced4ffe008eb6fb01a421
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-03-07 14:36:14 +0100

    Adding Data To Json

--- src/Components/Home/Header/Swiper/Swiper.jsx
+++ src/Components/Home/Header/Swiper/Swiper.jsx
@@ -1,4 +1,4 @@
-import Data from "../../../Data/db.json";
+import Data from "../../../../Data/db.json";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import { LazyLoadImage } from "react-lazy-load-image-component";
