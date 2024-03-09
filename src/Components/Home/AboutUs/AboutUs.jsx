c0ec83628af00c567cf2cbe4ba39adc32cf01a76
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-03-09 12:08:11 +0100

    Add Links To The Buttons

--- src/Components/Home/AboutUs/AboutUs.jsx
+++ src/Components/Home/AboutUs/AboutUs.jsx
@@ -122,18 +122,6 @@ const AboutUs = () => {
                   </div>
                 </motion.div>
               </div>
-
-              <motion.div
-                initial="hidden"
-                whileInView="visible"
-                viewport={{ once: true, amount: 0.5 }}
-                transition={{ duration: 0.5 }}
-                variants={{
-                  hidden: { opacity: 0, y: 120 },
-                  visible: { opacity: 1, y: 0 },
-                }}
-                className="textCenter"
-              ></motion.div>
             </div>
           </div>
         </div>
