dcf661d5991aa78f9098470aa9f63eba39879493
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-03-09 13:22:32 +0100

    Turn Images To webp

--- src/Components/Home/Header/Swiper/Swiper.jsx
+++ src/Components/Home/Header/Swiper/Swiper.jsx
@@ -4,40 +4,47 @@ import { Autoplay } from "swiper/modules";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "swiper/css";
 import "./Swiper.css";
-import ImgOne from "../../../../assets/HomePageOne.jpg";
-import ImgTwo from "../../../../assets/HomePageTwo.jpg";
-import ImgThree from "../../../../assets/HomePageThree.jpg";
+import ImgOne from "../../../../assets/HomePageOne.webp";
+import ImgTwo from "../../../../assets/HomePageTwo.webp";
+import ImgThree from "../../../../assets/HomePageThree.webp";
 import Check from "../../../../assets/check.png";
-import { useTranslation } from "react-i18next";
+// import { useTranslation } from "react-i18next";
 
-const DataImgs = [
+const data = [
   {
+    id: 1,
+    heading: "Immigration visa Consulting",
+    heading_ar: "This Text Is On Arabic",
+    paragraph_ar: "This Paragraph Is On Arabic",
+
+    paragraph: "Expert Guidance for a Seamless Immigration Journey",
+    paragraph_check: "Meeting Your Unique Needs",
+    paragraph_check_ar: "This Check Paragraph is on arabic",
+
     Img: ImgOne,
     alt: "Background One",
   },
 
   {
+    id: 2,
+    heading: "Immigration visa Consulting",
+    paragraph: "Expert Guidance for a Seamless Immigration Journey",
+    paragraph_check: "Meeting Your Unique Needs",
+
     Img: ImgTwo,
     alt: "Background Two",
   },
 
   {
+    id: 3,
+    heading: "Immigration visa Consulting",
+    paragraph: "Expert Guidance for a Seamless Immigration Journey",
+    paragraph_check: "Meeting Your Unique Needs",
+
     Img: ImgThree,
     alt: "Background Three",
   },
 ];
-
-const Images = DataImgs.map(({ Img, alt }, i) => {
-  return (
-    <div key={i}>
-      <LazyLoadImage
-        className="h-[75vh] w-full md:h-[100vh]"
-        src={Img}
-        alt={alt}
-      />
-    </div>
-  );
-});
 
 const Paragraphs = Data.Home_Header_Swipper.map(({ id, paragraph_check }) => {
   return (
@@ -76,30 +83,30 @@ const Buttons = Data.Home_Header_Button.map(
 );
 
 export default function App() {
-  const { i18n } = useTranslation();
+  // const { i18n } = useTranslation();
 
-  const data = Data.Home_Header_Swipper.map((data) => {
-    if (i18n.language == "ar") {
-      return {
-        id: data.id,
-        heading: data.heading_ar,
-        paragraph: data.paragraph_ar,
-        paragraph_check: data.paragraph_check_ar,
-      };
-    }
+  // const data = Data.Home_Header_Swipper.map((data) => {
+  //   if (i18n.language == "ar") {
+  //     return {
+  //       id: data.id,
+  //       heading: data.heading_ar,
+  //       paragraph: data.paragraph_ar,
+  //       paragraph_check: data.paragraph_check_ar,
+  //     };
+  //   }
 
-    // if (i18n.language == 'fr') {
-    //   return {
-    //     id: teacher.id,
-    //     image: teacher.image,
-    //     job: teacher.job_fr,
-    //     name: teacher.name_fr,
-    //     introduction: teacher.introduction_fr,
-    //   };
-    // }
+  // if (i18n.language == 'fr') {
+  //   return {
+  //     id: teacher.id,
+  //     image: teacher.image,
+  //     job: teacher.job_fr,
+  //     name: teacher.name_fr,
+  //     introduction: teacher.introduction_fr,
+  //   };
+  // }
 
-    return data;
-  });
+  // return data;
+  // });
 
   return (
     <>
@@ -114,10 +121,14 @@ export default function App() {
           modules={[Autoplay]}
           className="mySwiper"
         >
-          {data.map(({ id, heading, paragraph }) => {
+          {data.map(({ id, heading, paragraph, Img, alt }) => {
             return (
               <SwiperSlide key={id} className="relative text-white">
-                {Images}
+                <LazyLoadImage
+                  className="h-[75vh] w-full md:h-[100vh]"
+                  src={Img}
+                  alt={alt}
+                />
                 <div className="absolute w-full md:w-1/2 p-8 lg:px-24 top-[15%] sm:top-[20%]">
                   <div>
                     <h1 className="mb-4 md:text-5xl">{heading}</h1>
