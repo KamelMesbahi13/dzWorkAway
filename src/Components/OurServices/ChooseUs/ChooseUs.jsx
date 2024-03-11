79c602c9171dc3b3e61a49935436b25564bcc6ce
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-03-11 17:00:01 +0100

    Fixing Responsive ChooseUs rtl

--- src/Components/OurServices/ChooseUs/ChooseUs.jsx
+++ src/Components/OurServices/ChooseUs/ChooseUs.jsx
@@ -5,9 +5,66 @@ import ImgThree from "../../../assets/Support.png";
 import ImgFour from "../../../assets/QualityService.png";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { motion } from "framer-motion";
+import { useTranslation } from "react-i18next";
 
-const HeadingsAndParagraphsOne =
-  Data.OurServices_ChooseUs__Heading_Paragraph_One.map(
+const data = [
+  {
+    id: 1,
+    Icon: ImgOne,
+    Heading: "Team",
+    Heading_ar: "Team",
+    Heading_fr: "Team",
+  },
+
+  {
+    id: 2,
+    Icon: ImgTwo,
+    Heading: "CheckList",
+    Heading_ar: "CheckList",
+    Heading_fr: "CheckList",
+  },
+
+  {
+    id: 3,
+    Icon: ImgThree,
+    Heading: "Support",
+    Heading_ar: "Support",
+    Heading_fr: "Support",
+  },
+
+  {
+    id: 4,
+    Icon: ImgFour,
+    Heading: "Quality",
+    Heading_ar: "Quality",
+    Heading_fr: "Quality",
+  },
+];
+
+const ChooseUs = () => {
+  const { i18n } = useTranslation();
+
+  const modifiedDataHeader =
+    Data.OurServices_ChooseUs__Heading_Paragraph_One.map((data) => {
+      if (i18n.language === "ar") {
+        return {
+          id: data.id,
+          header: data.header_ar,
+          description: data.description_ar,
+        };
+      }
+
+      if (i18n.language === "fr") {
+        return {
+          id: data.id,
+          header: data.header_fr,
+          description: data.description_fr,
+        };
+      }
+      return data;
+    });
+
+  const HeadingsAndParagraphsOne = modifiedDataHeader.map(
     ({ id, header, description }) => {
       return (
         <div key={id}>
@@ -25,8 +82,29 @@ const HeadingsAndParagraphsOne =
     }
   );
 
-const HeadingsAndParagraphsTwo =
-  Data.OurServices_ChooseUs__Heading_Paragraph_Two.map(
+  // ---------------------------------------------------------
+
+  const modifiedDataHeaderTwo =
+    Data.OurServices_ChooseUs__Heading_Paragraph_Two.map((data) => {
+      if (i18n.language === "ar") {
+        return {
+          id: data.id,
+          header: data.header_ar,
+          description: data.description_ar,
+        };
+      }
+
+      if (i18n.language === "fr") {
+        return {
+          id: data.id,
+          header: data.header_fr,
+          description: data.description_fr,
+        };
+      }
+      return data;
+    });
+
+  const HeadingsAndParagraphsTwo = modifiedDataHeaderTwo.map(
     ({ id, header, description }) => {
       return (
         <div key={id}>
@@ -43,33 +121,6 @@ const HeadingsAndParagraphsTwo =
     }
   );
 
-const data = [
-  {
-    id: 1,
-    Icon: ImgOne,
-    Heading: "Team",
-  },
-
-  {
-    id: 2,
-    Icon: ImgTwo,
-    Heading: "CheckList",
-  },
-
-  {
-    id: 3,
-    Icon: ImgThree,
-    Heading: "Support",
-  },
-
-  {
-    id: 4,
-    Icon: ImgFour,
-    Heading: "Quality",
-  },
-];
-
-const ChooseUs = () => {
   return (
     <>
       <motion.div
@@ -86,7 +137,7 @@ const ChooseUs = () => {
         <div>
           <div className="w-full md:w-2/4">{HeadingsAndParagraphsOne}</div>
           <div className="flex flex-col justify-between py-4 mt-12 duration-500 shadow-lg md:py-0 hover:shadow-xl bg-grey md:mt-16 md:flex-row">
-            <div className="w-full pl-4 md:w-1/2 md:mt-20">
+            <div className="w-full ltr:pl-4 rtl:pr-4 md:w-1/2 md:mt-20">
               {HeadingsAndParagraphsTwo}
             </div>
 
