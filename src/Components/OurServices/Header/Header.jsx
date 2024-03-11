6487d628a1d7ffc03e13e6e71f945412d9635156
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-03-11 16:14:23 +0100

    Fixing Responsive Our Services Header rtl

--- src/Components/OurServices/Header/Header.jsx
+++ src/Components/OurServices/Header/Header.jsx
@@ -1,8 +1,37 @@
+import Data from "../../../data.json";
 import Img from "../../../assets/ServicesBack.jpg";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { motion } from "framer-motion";
+import { useTranslation } from "react-i18next";
 
 const Header = () => {
+  const { i18n } = useTranslation();
+
+  const modifiedDataTitle = Data.OurServices_Header.map((data) => {
+    if (i18n.language === "ar") {
+      return {
+        id: data.id,
+        header: data.header_ar,
+      };
+    }
+
+    if (i18n.language === "fr") {
+      return {
+        id: data.id,
+        header: data.header_fr,
+      };
+    }
+    return data;
+  });
+
+  const Title = modifiedDataTitle.map(({ id, header }) => {
+    return (
+      <div key={id}>
+        <h1>{header}</h1>
+      </div>
+    );
+  });
+
   return (
     <motion.div
       name="services"
@@ -23,10 +52,9 @@ const Header = () => {
           alt="About US Background"
         />
       </div>
-      <div className="absolute pl-8 text-white -translate-y-1/4 md:pl-24 top-1/2">
-        <div>
-          <h1>Our Services</h1>
-        </div>
+      <div className="absolute text-white rtl:pr-8 ltr:pl-8 ltr:md:pl-20 rtl:md:pr-20 -translate-y-1/4 md:pl-24 top-1/2">
+        <div>{Title}</div>
+
         <div className="flex mt-1 md:mt-2">
           <div className="w-8 h-1 md:w-12 bg-secondColor"></div>
           <div className="flex ml-1">
