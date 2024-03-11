e5ca6336cbd24e6b1f327ed9d6fab52652f164ec
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-03-11 13:58:04 +0100

    Fixing Responsive Testimonial WhyUs rtl

--- src/Components/AboutUs/Testimonials/Testimonials.jsx
+++ src/Components/AboutUs/Testimonials/Testimonials.jsx
@@ -1,27 +1,52 @@
 import Data from "../../../data.json";
 import Swiper from "./Swiper/Swiper";
 import { motion } from "framer-motion";
+import { useTranslation } from "react-i18next";
 
-const HeadingsAndParagraphs = Data.AboutUs_Testimonial_Heading_Paragraph.map(
-  ({ id, header, description }) => {
-    return (
-      <div key={id}>
-        <div>
+const Testimonials = () => {
+  const { i18n } = useTranslation();
+
+  const modifiedDataHeaderAndParagraph =
+    Data.AboutUs_Testimonial_Heading_Paragraph.map((data) => {
+      if (i18n.language === "ar") {
+        return {
+          id: data.id,
+          header: data.header_ar,
+          description: data.description_ar,
+        };
+      }
+
+      if (i18n.language === "fr") {
+        return {
+          id: data.id,
+          header: data.header_fr,
+          description: data.description_fr,
+        };
+      }
+      return data;
+    });
+
+  const HeadingsAndParagraphs = modifiedDataHeaderAndParagraph.map(
+    ({ id, header, description }) => {
+      return (
+        <div key={id}>
           <div>
             <div>
-              <h1 className="relative mb-2 lg:headingStyleLg">{header}</h1>
-            </div>
-            <div>
-              <p>{description}</p>
+              <div>
+                <h1 className="relative mb-2 lg:mb-0 before:absolute before:hidden rtl:lg:pr-20 ltr:lg:pl-20 before:md:block before:h-2 before:w-16 before:rtl:left-[93%] before:left-0 before:bg-hoverColor before:top-1/2 before:-translate-y-1/2">
+                  {header}
+                </h1>
+              </div>
+              <div>
+                <p>{description}</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    );
-  }
-);
+      );
+    }
+  );
 
-const Testimonials = () => {
   return (
     <div className="mt-12 md:mt-28">
       <div>
