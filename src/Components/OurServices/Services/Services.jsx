3e0590c07daf2cf09b272fec6e85244996c277e1
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-03-11 17:40:57 +0100

    Fixing Responsive Services Services rtl

--- src/Components/OurServices/Services/Services.jsx
+++ src/Components/OurServices/Services/Services.jsx
@@ -1,3 +1,4 @@
+import Data from "../../../data.json";
 import ImgOne from "../../../assets/Experience.png";
 import ImgTwo from "../../../assets/Priority.png";
 import ImgThree from "../../../assets/Advice.png";
@@ -5,13 +6,21 @@ import ImgFour from "../../../assets/Support.png";
 import Shape from "../../../assets/OurServicesShape.jpg";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { motion } from "framer-motion";
+import { useTranslation } from "react-i18next";
 
 const data = [
   {
     id: 1,
     Img: ImgOne,
     Heading: "Document Preparation",
+    Heading_ar: "تلاسلقس مسهلتصح",
+    Heading_fr: "Document Preparation",
+
     Description:
+      "With extensive experience guiding countless clients, we offer seasoned expertise to navigate your journey seamlessly. Trust in our track record and let us streamline your path to success.",
+    Description_ar:
+      "انستمب سبخهلا سبخهالس شخهاب شخيهال شيهخال شخهشثال شحçاض بهاس هيسال سخيهلا ",
+    Description_fr:
       "With extensive experience guiding countless clients, we offer seasoned expertise to navigate your journey seamlessly. Trust in our track record and let us streamline your path to success.",
   },
 
@@ -19,33 +28,112 @@ const data = [
     id: 2,
     Img: ImgTwo,
     Heading: "Career Counseling",
+    Heading_ar: "مسايلاص ضخبهتسش",
+    Heading_fr: "Career Counseling",
+
     Description:
       "Your needs come first. We prioritize understanding your goals, challenges, and aspirations to deliver tailored solutions and exceptional service, ensuring your satisfaction and success.",
+
+    Description_ar:
+      "انستمب سبخهلا سبخهالس شخهاب شخيهال شيهخال شخهشثال شحçاض بهاس هيسال سخيهلا ",
+    Description_fr:
+      "With extensive experience guiding countless clients, we offer seasoned expertise to navigate your journey seamlessly. Trust in our track record and let us streamline your path to success.",
   },
 
   {
     id: 3,
     Img: ImgThree,
     Heading: "Visa Guidance",
+    Heading_ar: "هسقهالس شبضضب",
+    Heading_fr: "Visa Guidance",
+
     Description:
       "Trust our guidance. We offer tailored advice to help you navigate life's complexities with confidence and clarity.",
+    Description_ar:
+      "انستمب سبخهلا سبخهالس شخهاب شخيهال شيهخال شخهشثال شحçاض بهاس هيسال سخيهلا ",
+    Description_fr:
+      "With extensive experience guiding countless clients, we offer seasoned expertise to navigate your journey seamlessly. Trust in our track record and let us streamline your path to success.",
   },
 
   {
     id: 4,
     Img: ImgFour,
     Heading: "Application Assistance",
+    Heading_ar: "ضضبلثفي بيخنش يبثض",
+    Heading_fr: "Application Assistance",
+
     Description:
       "Our support is unwavering. We're here to guide you through challenges and celebrate your successes, offering personalized assistance every step of the way.",
+    Description_ar:
+      "انستمب سبخهلا سبخهالس شخهاب شخيهال شيهخال شخهشثال شحçاض بهاس هيسال سخيهلا ",
+    Description_fr:
+      "With extensive experience guiding countless clients, we offer seasoned expertise to navigate your journey seamlessly. Trust in our track record and let us streamline your path to success.",
   },
 ];
 
 const WhyUs = () => {
+  const { i18n } = useTranslation();
+
+  const modifiedDataHeader = Data.OurServices_Services_Header.map((data) => {
+    if (i18n.language === "ar") {
+      return {
+        id: data.id,
+        header: data.header_ar,
+        description: data.description_ar,
+      };
+    }
+
+    if (i18n.language === "fr") {
+      return {
+        id: data.id,
+        header: data.header_fr,
+        description: data.description_fr,
+      };
+    }
+    return data;
+  });
+
+  const modifiedDataContent = data.map((data) => {
+    if (i18n.language === "ar") {
+      return {
+        id: data.id,
+        Heading: data.Heading_ar,
+        Description: data.Description_ar,
+        Img: data.Img,
+      };
+    }
+
+    if (i18n.language === "fr") {
+      return {
+        id: data.id,
+        Heading: data.Heading_fr,
+        Description: data.Description_fr,
+        Img: data.Img,
+      };
+    }
+    return data;
+  });
+
+  const Heading = modifiedDataHeader.map(({ id, header, description }) => {
+    return (
+      <div key={id}>
+        <div>
+          <h1 className="relative mb-2 lg:mb-0 md:headingStyleMd lg:headingStyleLg">
+            {header}
+          </h1>
+        </div>
+        <div>
+          <p className="md:mt-4 lg:mt-0">{description}</p>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <>
       <div>
         <div className="container flex flex-col mt-20 lg:flex-row">
-          <div className="w-full lg:mr-8 lg:w-3/4">
+          <div className="w-full lg:ltr:mr-8 lg:w-3/4">
             <motion.div
               initial="hidden"
               whileInView="visible"
@@ -56,19 +144,7 @@ const WhyUs = () => {
                 visible: { opacity: 1, x: 0 },
               }}
             >
-              <div>
-                <h1 className="relative mb-2 lg:mb-0 md:headingStyleMd lg:headingStyleLg">
-                  Our Services
-                </h1>
-              </div>
-              <div>
-                <p className="md:mt-4 lg:mt-0">
-                  At DZ Work Away, we understand that the decision to immigrate
-                  is one of the most significant steps you will ever take. That
-                  is why we are committed to making your journey as smooth and
-                  successful as possible. Here is why you should choose us:
-                </p>
-              </div>
+              {Heading}
             </motion.div>
           </div>
 
@@ -83,7 +159,7 @@ const WhyUs = () => {
             }}
             className="grid w-full grid-cols-1 mt-8 lg:mt-32 gap-y-6 lg:gap-20 lg:grid-cols-2"
           >
-            {data.map(({ id, Img, Heading, Description }) => {
+            {modifiedDataContent.map(({ id, Img, Heading, Description }) => {
               return (
                 <div key={id}>
                   <div>
