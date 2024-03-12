8bb749db42b5304d66f71905d3f2a02a48670eb3
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-03-12 12:34:42 +0100

    Fixing Responsive Services Platform rtl

--- src/Components/OurServices/PlatformsAndServices/Platforms.jsx
+++ src/Components/OurServices/PlatformsAndServices/Platforms.jsx
@@ -10,18 +10,35 @@ const data = [
   {
     id: 1,
     header: "All The World",
+    header_ar: "النيب سههص شيابض",
+    header_fr: "All The World",
+
     Img: ImgOne,
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus asperiores nihil repudiandae, ut est maiores quam nostrum fugiat earum laboriosam.",
+    description_ar:
+      "يبساينلار بسنترا سبتمايئن اكسبيعال سبخلهتاىشي لشخيهئالس لاخهيالش ثخهلاسبلا سيخلعاس لخقثعللاق صقخعللاخ شيخبالقل خشثهلاقل سقخعلسق خعلاسحرلا سخبعلاصقخعل سخعللاسيهعر سعلرسلااب شخعثبش خعيب",
+    description_fr:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus asperiores nihil repudiandae, ut est maiores quam nostrum fugiat earum laboriosam.",
     button: "Apply",
+    button_ar: "التيهضه",
+    button_fr: "Apply",
   },
   {
     id: 2,
     header: "Canada",
+    header_ar: "النيب شيابض",
+    header_fr: "All The World",
     Img: ImgTwo,
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus asperiores nihil repudiandae, ut est maiores quam nostrum fugiat earum laboriosam.",
+    description_ar:
+      "يبساينلار بسنترا سبتمايئن اكسبيعال سبخلهتاىشي لشخيهئالس لاخهيالش ثخهلاسبلا سيخلعاس لخقثعللاق صقخعللاخ شيخبالقل خشثهلاقل سقخعلسق خعلاسحرلا سخبعلاصقخعل سخعللاسيهعر سعلرسلااب شخعثبش خعيب",
+    description_fr:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus asperiores nihil repudiandae, ut est maiores quam nostrum fugiat earum laboriosam.",
     button: "Apply",
+    button_ar: "التيهضه",
+    button_fr: "Apply",
   },
 ];
 
@@ -64,6 +81,29 @@ const Platforms = () => {
     }
   );
 
+  const modifiedDataContent = data.map((data) => {
+    if (i18n.language === "ar") {
+      return {
+        id: data.id,
+        header: data.header_ar,
+        description: data.description_ar,
+        Img: data.Img,
+        button: data.button_ar,
+      };
+    }
+
+    if (i18n.language === "fr") {
+      return {
+        id: data.id,
+        header: data.header_fr,
+        description: data.description_fr,
+        Img: data.Img,
+        button: data.button_fr,
+      };
+    }
+    return data;
+  });
+
   return (
     <div className="mt-28">
       <motion.div
@@ -80,51 +120,53 @@ const Platforms = () => {
       </motion.div>
       <div>
         <div className="flex flex-col md:mt-12 md:flex-row md:items-center md:justify-between textCenter">
-          {data.map(({ id, Img, header, description, button }) => {
-            return (
-              <div key={id}>
-                <motion.div
-                  initial="hidden"
-                  whileInView="visible"
-                  viewport={{ once: true, amount: 0.5 }}
-                  transition={{ duration: 0.5 }}
-                  variants={{
-                    hidden: { opacity: 0, y: 50 },
-                    visible: { opacity: 1, y: 0 },
-                  }}
-                >
-                  <div className="p-8 w-full mt-4 md:mt-0 md:w-[70%] shadow-lg duration-500 hover:shadow-xl hover:scale-[1.02]">
-                    <div>
+          {modifiedDataContent.map(
+            ({ id, Img, header, description, button }) => {
+              return (
+                <div key={id}>
+                  <motion.div
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ duration: 0.5 }}
+                    variants={{
+                      hidden: { opacity: 0, y: 50 },
+                      visible: { opacity: 1, y: 0 },
+                    }}
+                  >
+                    <div className="p-8 w-full mt-4 md:mt-0 md:w-[70%] shadow-lg duration-500 hover:shadow-xl hover:scale-[1.02]">
                       <div>
                         <div>
-                          <LazyLoadImage
-                            className="w-16 h-14"
-                            src={Img}
-                            alt={header}
-                          />
-                        </div>
-                        <div>
-                          <div className="mt-2">
-                            <h6>{header}</h6>
+                          <div>
+                            <LazyLoadImage
+                              className="w-16 h-14"
+                              src={Img}
+                              alt={header}
+                            />
                           </div>
                           <div>
-                            <p>{description}</p>
+                            <div className="mt-2">
+                              <h6>{header}</h6>
+                            </div>
+                            <div>
+                              <p>{description}</p>
+                            </div>
                           </div>
-                        </div>
-                        <div>
-                          <a rel="noreferrer" href={`/Nos-Platforms/${id}`}>
-                            <button className="mt-4 buttonCust">
-                              {button}
-                            </button>
-                          </a>
+                          <div>
+                            <a rel="noreferrer" href={`/Nos-Platforms/${id}`}>
+                              <button className="mt-4 buttonCust">
+                                {button}
+                              </button>
+                            </a>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                </motion.div>
-              </div>
-            );
-          })}
+                  </motion.div>
+                </div>
+              );
+            }
+          )}
         </div>
       </div>
       <Services />
