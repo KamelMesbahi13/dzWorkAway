d4fdcd1f66e4c490b8b6225c8032960f06640416
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-03-18 21:26:49 +0100

    Add Translation

--- src/Components/OurServices/PlatformsAndServices/Services.jsx
+++ src/Components/OurServices/PlatformsAndServices/Services.jsx
@@ -109,8 +109,8 @@ const Services = () => {
                 viewport={{ once: true, amount: 0.5 }}
                 transition={{ duration: 0.5 }}
                 variants={{
-                  hidden: { opacity: 0, x: -50 },
-                  visible: { opacity: 1, x: 0 },
+                  hidden: { opacity: 0 },
+                  visible: { opacity: 1 },
                 }}
                 className="mb-8"
               >
