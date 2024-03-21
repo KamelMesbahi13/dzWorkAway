d8e2fd8508685f34505b8aabd32f8d3db8092b94
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-03-21 20:00:39 +0100

    Adding Failed Component Code

--- src/Ui/Failed/Failed.jsx
+++ src/Ui/Failed/Failed.jsx
@@ -1,12 +1,37 @@
 import Icon from "../../assets/Failed.png";
 
 const Failed = () => {
+  const reload = () => {
+    location.reload();
+  };
+
   return (
     <div className="w-full md:w-2/4">
       <div className="container">
-        <div className="border shadow-xl border-grey">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore,
-          iste?
+        <div className="p-8 border shadow-xl border-grey">
+          <div>
+            <div className="my-4 md:my-8">
+              <div>
+                <img className="w-12 h-12" src={Icon} alt="Failed" />
+              </div>
+            </div>
+            <div>
+              <div>
+                <h6>Oops !!!</h6>
+              </div>
+              <div className="my-4 md:my-8">
+                <p>Something Went Wrong</p>
+              </div>
+            </div>
+            <div>
+              <button
+                onClick={reload}
+                className="text-white simpleButton bg-[#F15249] duration-300 hover:bg-[#FF0000]"
+              >
+                OK
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
